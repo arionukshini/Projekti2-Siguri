f512e8305d11cb5d56a1654a7325642643467b0a
Elisahadergjonaj <elisa.hadergjonaj@student.uni-pr.edu>
2026-04-15 21:27:24 +0200

    Add main encryption example

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 from otp_algorithm import OTPAlgorithm
 
-otpa = OTPAlgorithm("rrjetatkompjuterike")
+algo = OTPAlgorithm("mysecretseed")
+
+enc = algo.encrypt("Pershendetje")
+print(enc)
+
+dec = algo.decrypt(enc)
+print(dec)
