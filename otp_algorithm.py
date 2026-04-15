26e137e8329955d7db1685e293cc4163ce8b0b5a
Elisahadergjonaj <elisa.hadergjonaj@student.uni-pr.edu>
2026-04-15 21:20:37 +0200

    Add decrypt method and main example

--- otp_algorithm.py
+++ otp_algorithm.py
@@ -1,13 +1,17 @@
 from keystream import generate_keystream
 from otp_cipher import OTPCipher
 
+
 class OTPAlgorithm:
     def __init__(self, seed):
         self.seed = seed
-        
+
     def encrypt(self, plaintext):
-        ks = generate_keystream(len(plaintext.encode('utf-8')), self.seed)
+        ks = generate_keystream(len(plaintext.encode("utf-8")), self.seed)
         cipher = OTPCipher(ks)
         return cipher.encrypt(plaintext)
-    
-    
+
+    def decrypt(self, ciphertext):
+        ks = generate_keystream(len(ciphertext), self.seed)
+        cipher = OTPCipher(ks)
+        return cipher.decrypt(ciphertext)
