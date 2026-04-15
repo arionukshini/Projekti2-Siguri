091299a6bfb24185e4c3ad538bc50e142891a0cc
Elisahadergjonaj <elisa.hadergjonaj@student.uni-pr.edu>
2026-04-15 21:36:15 +0200

    Update main encryption text

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from otp_algorithm import OTPAlgorithm
 
 otpa = OTPAlgorithm("rrjetatkompjuterike")
 
-enc = otpa.encrypt("Pershendetje")
+enc = otpa.encrypt("Ky eshte implementimi i algoritmit One Time Pad ne Python.")
 print(enc)
 
 dec = otpa.decrypt(enc)
